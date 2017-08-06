42a68b08b7cb6f08e25f417798e323e8e6884548
Hoang Nguyen <mizore742@hotmail.com>
2017-08-06 14:26:06 -0400

    Added auto layout constraints to fit all iPhones and iPads.

--- lionsApp/HomeViewController.swift
+++ lionsApp/HomeViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 import TwitterKit
 
 class FirstViewController: TWTRTimelineViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let client = TWTRAPIClient()
         self.dataSource = TWTRListTimelineDataSource(listSlug: "COHSLions", listOwnerScreenName: "cohs_lions", apiClient: client)
-        self.tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        // self.tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        self.title = "Events"
     }
+    
 }
