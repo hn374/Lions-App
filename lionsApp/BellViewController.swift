abe8e48644744d8093d131d23cf810e336b75d6b
Hoang Nguyen <mizore742@hotmail.com>
2017-08-21 00:28:40 -0400

    Added all the code for the bell schedule controller.

--- lionsApp/BellViewController.swift
+++ lionsApp/BellViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class BellViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Bell Schedule"
-
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
+    }
+    
+    @IBAction func rScheduleTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "rSchedule")
+    }
+    
+    @IBAction func xScheduleTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "xSchedule")
+    }
+    
+    @IBAction func mScheduleTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "mSchedule")
+    }
+    
+    @IBAction func aScheduleTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "aSchedule")
+    }
+    
+    @IBAction func dScheduleTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "dSchedule")
     }
     
 
