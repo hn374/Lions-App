f5ecbd0b252e4b2c8849b3428ac53382fc2ed118
Hoang Nguyen <mizore742@hotmail.com>
2017-08-21 00:10:09 -0400

    Added auto layout for the image view in Bell Schedule. Just added all the buttons but did not include their auto layout constraints yet.

--- lionsApp/BellViewController.swift
+++ lionsApp/BellViewController.swift
@@ -12,13 +12,13 @@ class BellViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Bell Schedule"
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
 
