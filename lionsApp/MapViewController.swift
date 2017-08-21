5e94b5800dd03f60f3425a4fead6bab38203e6c1
Hoang Nguyen <mizore742@hotmail.com>
2017-08-20 23:51:13 -0400

    Finished MapViewer storyboard. Completed the auto layout for buttons and images to fit all sizes. Added all the buttons and imageView to the MapViewController. Added code to change images of the imageView corresponding to each button.

--- lionsApp/MapViewController.swift
+++ lionsApp/MapViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Maps"
@@ -18,5 +20,22 @@ class MapViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+   
+    @IBAction func firstFloorTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "firstFloor")
+    }
 
+    @IBAction func secondFloorTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "secondFloor")
+    }
+    
+    @IBAction func thirdFloorTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "thirdFloor")
+    }
+    
+    @IBAction func basementTapped(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "basement")
+    }
+    
+    
 }
